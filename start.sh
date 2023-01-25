19319c8ffde0b47ad81f22f819ca9860156815ce
Hanpeng <joapolarbear@gmail.com>
2023-01-25 16:28:59 +0800

    Update start.sh
    Using 'rm -rf' safely

--- start.sh
+++ start.sh
@@ -32,3 +32,5 @@ tmux source-file ~/.tmux.conf
 ### Config On-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
+### Using 'rm -rf' safely
+alias rm='rm -i'
