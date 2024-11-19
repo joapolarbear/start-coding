eef28bca638c70cd3d4cc4adeb0f3d4ad576039b
Hanpeng <joapolarbear@gmail.com>
2024-11-19 10:22:27 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,6 +8,11 @@ case "$uname" in
     (*) echo 'error: unsupported platform.'; exit 2; ;;
 esac;
 
+print_blue() {
+    local str="$1"
+    echo -e "\e[34m$str\e[0m"
+}
+
 usrname=$(whoami)
 if [ $usrname != "root" ]; then
     sudo_prefix="sudo"
@@ -17,7 +22,7 @@ fi
 $sudo_prefix $installCMD update && $sudo_prefix $installCMD install -y vim git wget tmux zsh vim-gtk
 
 ### Config Vim
-echo "Config Vim ..."
+print_blue "Config Vim ..."
 if [ ! -d "~/.vim_runtime" ]; then
     # git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
     cp -r 3rdparty/vimrc ~/.vim_runtime
@@ -26,11 +31,12 @@ sh ~/.vim_runtime/install_awesome_vimrc.sh
 cp vim/my_configs.vim ~/.vim_runtime/
 
 ### Config Tmux
-echo "Config Tmux ..."
+print_blue "Config Tmux ..."
 cp tmux/.tmux.conf ~/
 tmux source-file ~/.tmux.conf
 
 ### Config On-my-zsh
+print_blue "Config On-my-zsh"
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 ### Using 'rm -rf' safely
